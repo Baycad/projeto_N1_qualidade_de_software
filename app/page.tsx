726af43e9f29d6775be2cfdcc68645d2422069d6
JoaoGln <jgjln2006@gmail.com>
2025-09-30 16:28:14 -0300

    Commit 4: Definindo a fonte de vídeo para o Background

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Background } from "@/components/background"
-+import { Newsletter } from "@/components/newsletter"
+ import { Newsletter } from "@/components/newsletter"
  
  export default function Home() {
    return (
      <main className="p-inset h-[100dvh] w-full">
        <div className="relative h-full w-full">
-         <Background />
-+        <Newsletter />
+-        <Background />
++        <Background src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/alt-g7Cv2QzqL3k6ey3igjNYkM32d8Fld7.mp4" />
+         <Newsletter />
        </div>
      </main>
    )

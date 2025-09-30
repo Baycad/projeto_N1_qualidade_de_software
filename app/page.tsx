0617aded297ecc5f802cee51f18c7cde34a4f5ba
JoaoGln <jgjln2006@gmail.com>
2025-09-30 16:27:32 -0300

    Commit 3: Adicionando o componente Newsletter à página Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-+import { Background } from "@/components/background"
-
+import { Background } from "@/components/background"
++import { Newsletter } from "@/components/newsletter"
+ 
  export default function Home() {
    return (
      <main className="p-inset h-[100dvh] w-full">
        <div className="relative h-full w-full">
-+        <Background />
+         <Background />
++        <Newsletter />
        </div>
      </main>
    )

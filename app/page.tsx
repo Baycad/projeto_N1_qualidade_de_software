1dd8190ddc8668a19b998d2ce7827bd27b5983b5
JoaoGln <jgjln2006@gmail.com>
2025-09-30 16:25:49 -0300

    Commit 1: Estruturando a página Home inicial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="p-inset h-[100dvh] w-full">
+      <div className="relative h-full w-full">
+        {/* Componentes serão adicionados aqui */}
+      </div>
+    </main>
+  )
+}

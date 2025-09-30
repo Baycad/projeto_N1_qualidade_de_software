72cec99b0830bf50ba7bcaab0b5ebe1f64c53a15
JoaoGln <jgjln2006@gmail.com>
2025-09-30 16:26:49 -0300

    Commit 2: Adicionando o componente Background à página Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-export default function Home() {
-  return (
-    <main className="p-inset h-[100dvh] w-full">
-      <div className="relative h-full w-full">
-        {/* Componentes serão adicionados aqui */}
-      </div>
-    </main>
-  )
-}
++import { Background } from "@/components/background"
+
+ export default function Home() {
+   return (
+     <main className="p-inset h-[100dvh] w-full">
+       <div className="relative h-full w-full">
++        <Background />
+       </div>
+     </main>
+   )
+ }
